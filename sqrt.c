74a4dc0929f8816218e02cf5890ba96c0110478b
rahul-a5 <myrahulanand20@gmail.com>
2020-10-15 20:35:16 +0530

    DEBUG: added

--- sqrt.c
+++ sqrt.c
@@ -8,19 +8,19 @@ positive number. */
 int main(int argc, char* argv[]) {
 	
 	if (argc != 2) {
-		printf("NEGSUPPORT:Usage: sqrt input\n\n");
+		printf("DEBUG:Usage: sqrt input\n\n");
 		exit(-1);
 	}
 
 	int input = atoi(argv[1]);
 	if(input>=0){
-	printf("NEGSUPPORT:Sqrt of %d is %f\n",input,sqrt(input));
+	printf("DEBUG:Sqrt of %d is %f\n",input,sqrt(input));
 	}
 	else{
-	printf("NEGSUPPORT:Sqrt of %d is %fi\n",input,sqrt(-1*input));
+	printf("DEBUG:Sqrt of %d is %fi\n",input,sqrt(-1*input));
 	}
-	printf("NEGSUPPORT:End of program. Exiting\n");
-	printf("NEGSUPPORT:Byee\n");
+	printf("DEBUG:End of program. Exiting\n");
+	printf("DEBUG:Byee\n");
 	return(0);
 
 } // end main
